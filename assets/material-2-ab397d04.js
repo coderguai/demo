d75766d43d2b021758830eaaa1fdd27ac08ed67a
guai <coderguai@gmail.com>
2023-09-11 17:26:29 +0800

    Initialize gh-page

---
+++ assets/material-2-ab397d04.js
@@ -0,0 +1,1 @@
+const r=""+new URL("material-2-3f7f89f6.png",import.meta.url).href;export{r as _};
---
+++ assets/onWindowResize-9b069d67.js
@@ -0,0 +1,1 @@
+import{au as i}from"./index-81730b21.js";function n(e){i(()=>window,"resize",e,{passive:!0}),i(()=>window,"orientationchange",e,{passive:!0})}export{n as o};
